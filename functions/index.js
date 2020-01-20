9ebf05fa2686ee17a7762e668c2c194eca48d9e9
Alfie <corehallabusiness@gmail.com>
2020-01-20 21:40:34 +0100

    prettier

--- functions/index.js
+++ functions/index.js
@@ -3,14 +3,19 @@ const app = require('express')();
 
 app.use(require('./util/cors'));
 
-const { getUser, getAllUsers, createUser, addPlayer } = require('./handlers/users');
+const {
+	getUser,
+	getAllUsers,
+	createUser,
+	addPlayer
+} = require('./handlers/users');
 const { getPlayer, getAllPlayersInBracket } = require('./handlers/players');
 
 // User Routes
 app.get('/users/:userID', getUser);
 app.get('/users', getAllUsers);
 app.post('/users', createUser);
-app.post('/users/:userID/addPlayer', addPlayer)
+app.post('/users/:userID/addPlayer', addPlayer);
 
 // Player Routes
 
--- functions/util/admin.js
+++ functions/util/admin.js
@@ -2,8 +2,8 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./config/serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://rankxtest.firebaseio.com'
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://rankxtest.firebaseio.com'
 });
 
 db = admin.firestore();
