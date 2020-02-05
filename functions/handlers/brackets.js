9af126f2f0466b6f143be077139193ac23d25ed2
Alfie <corehallabusiness@gmail.com>
2020-02-05 20:50:37 +0100

    bracket schema

--- functions/handlers/brackets.js
+++ functions/handlers/brackets.js
@@ -1,1 +1,41 @@
 const { db } = require('../util/admin');
+const slugify = require('slugify');
+
+exports.createBracket = async (req, res) => {
+	try {
+		const { name, event } = req.params;
+		const slug = slugify(name, {
+			replacement: '-',
+			remove: /[*+~.()'"!:@]/g,
+			lower: true
+		});
+
+		const bracketDoc = await db
+			.collection('/bracket/')
+			.where('slug', '==', slug)
+			.where('event', '==', event)
+			.get();
+
+		if (bracketDoc.docs.length >= 0)
+			return res.status(400).json({ error: 'Bracket already exists' });
+
+		await db.doc(`/bracket/${bracket}`).set({
+			slug,
+			event,
+			name,
+			active: true,
+			whitelist: {
+				active: false,
+				users: []
+			},
+			blacklist: {
+				active: false,
+				users: []
+			}
+		});
+
+		return res.status(201).json('Bracket successfully created!');
+	} catch (e) {
+		return res.status(500).json({ error: e.code });
+	}
+};
--- functions/util/schema.js
+++ functions/util/schema.js
@@ -1,78 +1,90 @@
 const db = {
-    users: [
-        {
-            id: !ID, // Unique
-            name: !String,
-            socials: [
-                {
-                    name: !String,
-                    id: !String
-                }
-            ]
-        }
-    ],
-    players: [
-        {
-            id: !ID, // Unique
-            user: !ID,
-            bracket: !String,
-            ratings: ![!Number]
-        }
-    ],
-    queues: [
-        {
-            id: !ID, // Unique
-            user: !ID,
-            player: !ID,
-            bracket: !String,
-            active: !Boolean,
-            date: !String
-        }
-    ],
-    teams: [
-        {
-            id: !ID, // Unique
-            players: [
-                {
-                    id: !ID,
-                    rating: !Number
-                }
-            ],
-            bracket: !String
-        }
-    ],
-    matches: [
-        {
-            id: !ID, // Unique
-            teams: [
-                {
-                    players: [
-                        {
-                            id: !ID,
-                            rating: !Number
-                        }
-                    ],
-                    rating: !Number,
-                    score: !Number
-                }
-            ],
-            active: !Boolean,
-            confirmed: !Boolean
-        }
-    ],
-    brackets: [
-        {
-            slug: !String, // Unique
-            name: !String,
-            whitelist: {
-                active: !Boolean,
-                users: ![!ID] // Player IDs
-            },
-            blacklist: {
-                active: !Boolean,
-                users: ![!ID] // Player IDs
-            },
-            active: !Boolean
-        }
-    ]
-}
+	users: [
+		{
+			id: !ID, // Unique
+			name: !String,
+			socials: [
+				{
+					name: !String,
+					id: !String
+				}
+			]
+		}
+	],
+	players: [
+		{
+			id: !ID, // Unique
+			user: !ID,
+			bracket: !String,
+			ratings: ![!Number]
+		}
+	],
+	queues: [
+		{
+			id: !ID, // Unique
+			user: !ID,
+			player: !ID,
+			bracket: !String,
+			active: !Boolean,
+			date: !String
+		}
+	],
+	teams: [
+		{
+			id: !ID, // Unique
+			players: [
+				{
+					id: !ID,
+					rating: !Number
+				}
+			],
+			bracket: !String
+		}
+	],
+	matches: [
+		{
+			id: !ID, // Unique
+			teams: [
+				{
+					players: [
+						{
+							id: !ID,
+							rating: !Number
+						}
+					],
+					rating: !Number,
+					score: !Number
+				}
+			],
+			active: !Boolean,
+			confirmed: !Boolean
+		}
+	],
+	events: [
+		{
+			slug: !String, // Unique
+			name: !String
+		}
+	],
+	brackets: [
+		{
+			slug: !String, // Unique
+			event: !ID,
+			name: !String,
+			active: !Boolean,
+			whitelist: {
+				active: !Boolean,
+				users: ![!ID] // Player IDs
+			},
+			blacklist: {
+				active: !Boolean,
+				users: ![!ID] // Player IDs
+			},
+			specs: {
+				rating_system: !RatingSystem, // Fixed, Elo
+				starting_rating: !Number,
+				type: !BracketType // BH1v1, BH2v2
+			}
+		}
+	]
+};
